d65c264e278f133ca3414b3de5228f8dd7a2d974
Austin <agriffin7@stu.jsu.edu>
2018-10-04 03:42:27 -0500

    Added Src folder

---
+++ src/project/team/cs310/Controller.java
@@ -0,0 +1,6 @@
+package project.team.cs310;
+
+
+public class Controller {
+    
+}
---
+++ src/project/team/cs310/TimeClockProgram.java
@@ -0,0 +1,10 @@
+package project.team.cs310;
+
+//this is the main class
+public class TimeClockProgram {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
