aafb6d41d86498770e6cb9c2a54a2cc24e4a2715
Austin <agriffin7@stu.jsu.edu>
2018-10-12 13:55:13 -0500

    Split the model class objects all into their own class files. This will help with organization. All classes should still work fine. When you make edits, do them in your new file

--- src/project/team/cs310/TimeClockProgram.java
+++ src/project/team/cs310/TimeClockProgram.java
@@ -7,9 +7,6 @@ public class TimeClockProgram {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        TASDatabase db = new TASDatabase();
-        db.Connection();
-        db.getBadge("08D01475");
     }
     
 }
