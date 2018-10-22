644136e3ab746522b5debd5f7937b37534476f56
engrmshakir <mshakir25@hotmail.com>
2018-10-22 10:00:26 -0500

    TasLogic Updated

--- src/project/team/cs310/TASLogic.java
+++ src/project/team/cs310/TASLogic.java
@@ -1,13 +1,8 @@
-
 package project.team.cs310;
 
-
-public class TASLogic {
-    /*TASLogic will implement:
-        The business logic (such as computing the total minutes accrued by an employee within a single day)
-        The application logic (such as converting an ordered list of Punch objects to JSON format)
-        This class will initially be empty, so !!THERE IS NOTHING TO DO FOR THIS IN FEATURE !!; 
-        the business and application logic will be added as static methods during subsequent features.*/
-        
-        
+// Muhammad Shakir
+public class TASLogic 
+{
+    // Feature One Doesn't required to do anything in TASLogic.
+    // Need to add the logic in Feature 2,3,4, and 5
 }
