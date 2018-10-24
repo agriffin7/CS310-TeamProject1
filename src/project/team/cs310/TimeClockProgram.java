c1478ada842716a85e2130922698e1eb9d904e76
engrmshakir <mshakir25@hotmail.com>
2018-10-24 12:08:39 -0500

    Updated TasDatabase for feature 2 - Check the TimeClock.java

--- src/project/team/cs310/TimeClockProgram.java
+++ src/project/team/cs310/TimeClockProgram.java
@@ -19,19 +19,23 @@ public class TimeClockProgram
         Badge b1 = db.getBadge("12565C60");
         Badge b2 = db.getBadge("08D01475");
         Badge b3 = db.getBadge("D2CC71D4");
-        
+        Badge b4 = db.getBadge("4DAC9951");
         System.out.println(b1.toString());
         System.out.println(b2.toString());
         System.out.println(b3.toString());
+        System.out.println(b4.toString());
         
         Punch p1 = db.getPunch(3433);
-        Punch p2 = db.getPunch(3325);
-        Punch p3 = db.getPunch(1963);
-
+        Punch p2 = db.getPunch(3440);
+        Punch p3 = db.getPunch(3325);
+        Punch p4 = db.getPunch(1963);
+        Punch p5 = db.getPunch(5702);
         
         System.out.println(p1.printOriginalTimestamp());
         System.out.println(p2.printOriginalTimestamp());
         System.out.println(p3.printOriginalTimestamp());
+        System.out.println(p4.printOriginalTimestamp());
+        System.out.println(p5.printOriginalTimestamp());        
 
     }    
 }
