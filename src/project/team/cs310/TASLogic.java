5eb3104168727f399406de76a22d819334b8a8b8
Dalton Murphree <dmurphree2@stu.jsu.edu>
2018-11-14 11:57:28 -0600

    Accidentally passed a test in TASLogic

--- src/project/team/cs310/TASLogic.java
+++ src/project/team/cs310/TASLogic.java
@@ -1,9 +1,23 @@
 package project.team.cs310;
 
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.ArrayList;
 // Muhammad Shakir
 public class TASLogic 
 {
     // Feature One Doesn't required to do anything in TASLogic.
     // Need to add the logic in Feature 2,3,4, and 5
     // 
+    
+    public static int calculateTotalMinutes(ArrayList<Punch> dailypunchlist, Shift shift) { 
+        
+        
+        return 0;
+        
+}
 }
